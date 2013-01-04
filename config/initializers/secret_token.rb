97361ac28de98c33cf3e0799877b9bdde47d044a
Phillip Lackey <philliplackey@aimac.corp.athlon.com>
2013-01-04 15:13:08 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RustoleumInvite::Application.config.secret_token = '9dca382b2543cc3a497ee0fb5bae5866bc4bf693f2d4fe4ff00104bc44c1c7a9216647d9f7403323026ef8ff9630e0797762a57e225b729d9b0ab9a341262545'
